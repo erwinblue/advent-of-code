82df2a5548f753bce21676b2c7a86e7c35b3cc71
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-20 13:46:13 -0500

    Refactor the code for 2023 Day 1 Part 2 (unfinished still)

--- d01-2023/src/main.rs
+++ d01-2023/src/main.rs
@@ -1,235 +1,209 @@
-
+use core::num;
+/* *************************************************************************
+                           LIBRARIES AND DECLARATIONS           
+   ************************************************************************* */
+use std::collections::{BTreeMap, HashMap};
 use std::env;
-use std::fs::File;
+use std::fs:: File;
 use std::io::{BufRead, BufReader};
 
 
-#[derive(Debug)]
+
+/* *************************************************************************
+                              HELPER FUNCTIONS       
+   ************************************************************************* */
+
+fn get_digits<'a>(line: &'a str, is_spelled: bool) -> Option<Vec<usize>> {
+    // The BTreeMap will contain the first and last occurrences of the digits only
+    // since Rust only provides find and rfind functions and we are too lazy to 
+    // create a more complex pattern search of occurences.
+
+    let mut digits: BTreeMap<usize, usize> = BTreeMap::new();
+    let mut return_digits: Vec<usize> = Vec::new();
+
+    // Look for first occurrence of numeric digits and put in BTreeMap 
+    for (index, pattern) in vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].into_iter().enumerate() {
+        let x = match line.find(pattern) {
+            Some(y) => y,
+            None => continue
+        };
+        digits.insert(x, index);
+    }
+
+    // Look for first occurrence of string digits and put in BTreeMap if we consider spelled out numbers
+    if is_spelled {
+        for (index, pattern) in vec!["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].into_iter().enumerate() {
+            let x = match line.find(pattern) {
+                Some(y) => y,
+                None => continue
+            };
+            digits.insert(x, index);
+        }
+    }
+
+    // Now get the very first occurence of a digit
+    // BTreeMap auto-sorts by key hence just get the first entry since we have numbers for the keys
+    if digits.len() > 0 {
+        let (_, first_digit)= digits.pop_first().unwrap_or((0, 0));
+        return_digits.push(first_digit);
+    } else {
+        // If we reach this code then it means there was no digit parsed from the row of string
+        // Hence our last digit occurence won't matter.
+        return None;
+    }
+
+    // Reset our BTreeMap to get the last digit occurence
+    digits.clear();
+
+    // Look for last occurrence of numeric digits and put in BTreeMap 
+    for (index, pattern) in vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].into_iter().enumerate() {
+        let x = match line.rfind(pattern) {
+            Some(y) => y,
+            None => continue
+        };
+        digits.insert(x, index);
+    }
+
+    // Look for last occurrence of string digits and put in BTreeMap if we consider spelled out numbers
+    if is_spelled {
+        for (index, pattern) in vec!["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].into_iter().enumerate() {
+            let x = match line.rfind(pattern) {
+                Some(y) => y,
+                None => continue
+            };
+            digits.insert(x, index);
+        }
+    }
+
+    // Get the last digit occurence.  Again BTreeMap auto-sorts this collection hence
+    // just get the last element.  
+    if digits.len() > 0 {
+        // We can safely use unwrap_or()... since we tested for the HashMap lenght.
+        let (_, last_digit)= digits.pop_last().unwrap_or((0, 0));
+        return_digits.push(last_digit);
+    }
+
+    Some(return_digits)
+
+}
+
+/* *************************************************************************
+                            STRUCTURE AND METHODS
+   ************************************************************************* */
+
+/* -------------------------------------------------------------------------
+    CalibrationCode struct and functions
+    
+    A digit can be either a number spelled our or,
+    a numeric number from 0 to 9. 
+  -------------------------------------------------------------------------- */
+struct CalibrationCode {
+    first: usize,
+    last: usize,
+    value: u8
+}
+
+impl CalibrationCode {
+    fn new(f: usize, l: usize) -> CalibrationCode {
+        let mut v: String = String::new();
+        v.push_str(&f.to_string());
+        v.push_str(&l.to_string());
+        let c = v.to_string().parse::<u8>().unwrap();
+        return CalibrationCode {
+            first: f,
+            last: l,
+            value: c   
+        };
+    }
+}
+
+/* -------------------------------------------------------------------------
+    CalibrationRow struct and functions
+    
+    A row can have or not have any digits as numeric or spelled-out string form.
+    Hence, the digits attribute is an Option enum of a BTreeMap collection.
+    BTreeMap collection is chosen to be able to sort the digit index/position
+    on an given line of document or paragraph.
+  -------------------------------------------------------------------------- */
 struct CalibrationRow {
     line: String,
-    first: char,
-    last: char
+    code: Option<CalibrationCode>
 }
 
 impl CalibrationRow {
-    fn calibration_value(&self) -> u32 {
-        let c: String = [self.first, self.last].into_iter().collect();
-        c.to_string().parse::<u32>().unwrap_or(0)
+    // Create a new calibration row from a given line
+    fn from<'a>(input_line: &'a str, is_spelled: bool) -> CalibrationRow {
+        let digits = match get_digits(input_line, is_spelled) {
+            Some(d) => d,
+            None => {
+                return CalibrationRow {
+                    line: input_line.to_owned(),
+                    code: None
+                }
+            }
+        };
+        let c = CalibrationCode::new(
+            *digits.get(0).unwrap(),
+            *digits.get(1).unwrap()
+        );
+        CalibrationRow{
+            line: input_line.to_owned(),
+            code: Some(c)
+        }
     }
 }
 
+/* -------------------------------------------------------------------------
+    CalibrationDocument struct and fucntions
+    A document is a sequence of calibration rows 
+  -------------------------------------------------------------------------- */
 struct CalibrationDocument {
-    lines: Vec<CalibrationRow>
+    lines: Vec<CalibrationRow>,
+    spelled_digits: bool
 }
 
 impl CalibrationDocument {
-    fn document_total(&self) -> u32 {
-        let mut total: u32 = 0;
-        for row in self.lines.iter() {
-            total += row.calibration_value();
+    // Read-in the input file and parse each line
+    fn new(input_file: &str, is_spelled: bool) -> Result<CalibrationDocument, std::io::Error> {
+        let f = match File::open(input_file) {
+            Ok(k) => k,
+            Err(e) => return Err(e)
+        };
+        let mut c = CalibrationDocument { lines: vec![], spelled_digits: is_spelled };
+        for (_line_number, line_string) in BufReader::new(f).lines()
+            .map(|x| match x {
+                Ok(y) => y,
+                Err(_) => String::from("<CANNOT READ LINE>")
+            }).enumerate() {
+            let row = CalibrationRow::from(&line_string, is_spelled);
+            c.lines.push(row);
         }
+        Ok(c)
+    }
+
+    /*       ---------------------- TOOOOOOOOOO DOOOOOOOOOOOOOOOOOOO -----------------
+    /*  If spelled digits is false just consider numeric digits.  Otherwise consider both
+        numeric and spelled digits in the document */
+    fn spelled_digits(&mut self, flag: bool) {
+        self.spelled_digits = flag; 
+        // TODO: Re-read the document with spelled digits or not depending of flag value
+        for line in self.lines.iter() {
+            todo!();
+        }
+    }
+             ---------------------- TOOOOOOOOOO DOOOOOOOOOOOOOOOOOOO ----------------- */
+
+    fn document_total(&self) -> u32 {
+        let total = 0u32;
+        
         total
     }
-}
-
-#[derive(Clone)]
-struct Digit {
-    digit: String,
-    index: usize
-}
-
-
-/*
-    If the string does not have any digits return None
- */
-fn get_int_digit(s: &str, last: bool) -> Option<Digit> {
-    let mut v = Digit { digit: "0".to_string(), index: 0 };
-    let arr: Vec<char> = match last {
-        true => s.chars().into_iter().rev().collect(),
-        false => s.chars().into_iter().collect()
-    };
-    for (i, m) in arr.iter().enumerate() {
-        if m.is_digit(10) {
-            v.digit = m.to_string();
-            v.index  = i;
-            break;
-        }
-    }
-    println!("get_digit: {:?}, {:?}, {:?} {:?}", s, v.digit, v.index, last);
-    Some(v)
-}
-
-fn get_str_digit(s: &str, last: bool) -> Option<Digit> {
-    let mut v: Option<Digit> = None;
-    let mut found_digit = false;
-    let digits_vec: Vec<&str> = vec!["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    let digits_arr: Vec<&str> = vec!["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    for (i, p) in digits_vec.iter().enumerate() {
-        if ! last {
-            let num = match s.to_ascii_lowercase().find(*p) {
-                Some(k) => k,
-                None => continue
-            };
-            println!("Found first: {:?} {:?}", *p, num);
-            if v.is_none() {
-                v = Some(Digit {
-                    digit: digits_arr[i].to_owned(),
-                    index: num
-                });
-            } else {
-                if v.unwrap().index > num {
-                    let g = Digit {  
-                        digit: digits_arr[i].to_owned(),
-                        index: num
-                    };
-                    v = Some(g.clone());
-                } else {
-                    continue;
-                }
-            }
-            found_digit = true;
-        } else {
-            let num = match s.to_ascii_lowercase().rfind(*p) {
-                Some(k) =>  k,
-                None => continue
-            };
-            println!("Found last: {:?} {:?}", *p, num);
-            if v.is_none() {
-                v = Some(Digit {
-                    digit: digits_arr[i].to_owned(),
-                    index: num
-                });
-            } else {
-                if num > v.unwrap().index {
-                    let g = Digit {  
-                        digit: digits_arr[i].to_owned(),
-                        index: num
-                    };
-                    v = Some(g.clone());
-                } else {
-                    continue;
-                }
-            }
-            found_digit = true;
-        }
-    }
-    if found_digit {
-        println!("get_string_digit: {:?}, {:?}, {:?} {:?}", s, v.unwrap().digit, v.unwrap().index, last);
-        return v;
-    } else {
-        println!("get_string_digit: {:?}, n/a, n/a n/a", s);
-        return None;
-    }
-}
-
-fn get_real_digit(s: &str, last: bool) -> Option<Digit> {
-    let digit_int = get_int_digit(s, last);
-    let digit_str = get_str_digit(s, last);
-    let mut d = Digit { digit: '0', index: 0 };
-    if digit_int.is_some() && digit_str.is_some() {
-        if last {
-            if digit_int.as_ref().unwrap().index > digit_str.as_ref().unwrap().index {
-                d = digit_int.clone().unwrap();
-            } else {
-                d = digit_str.clone().unwrap();
-            }
-        } else {
-            if digit_int.as_ref().unwrap().index < digit_str.as_ref().unwrap().index {
-                d = digit_int.clone().unwrap();
-            } else {
-                d = digit_str.clone().unwrap();
-            }
-        }
-    } else if digit_int.is_some() {
-        d = digit_int.clone().unwrap();
-    } else if digit_str.is_some() {
-        d = digit_str.clone().unwrap();
-    } else {
-        return None;
-    }
-    println!("get_real_digit: {:?}, {:?}, {:?} {:?}", s, d.digit, d.index, last);
-    Some(d)
-}
-
-fn parse_input_line(s: &str) -> CalibrationRow {
-
-    let mut c = CalibrationRow { line: s.to_owned(), first: '0', last: '0'};
-
-    /* -------------------------------------------------------------------
-        Advent of Code 2023 Day 1 Part 1:
-          Calibration code is the first occurence of a number from beginning of the line
-          and the first occurence of a number from the end of the line.
-
-        Assumption in get_digit function for the first digit occurence is
-        that, if no digit is found, then last occurence is none as well
-        and it won't reach that part of the code.
-
-    let mut v = match get_int_digit(s, false) {
-        Some(x) => x,
-        None => return c
-    };
-    c.first = v.digit;
-
-    v = match get_int_digit(s, true) {
-        Some(x) => x,
-        None => return c
-    };
-    c.last = v.digit;
-    ------------------------------------------------------------------- */
-
-
-    /* -------------------------------------------------------------------
-        Advent of Code 2023 Day 1 Part 2:
-          Some digits are spelled out with letters: one, two, three, four,.., nine.
-          Recallibrate the parsing of the document
-
-        Same assumption, in get_real_digit function for the first digit occurence is
-        that, if no string digit is found, then last occurence is none as well
-        and it won't reach that part of the code.
-    ------------------------------------------------------------------- */
-
-    println!("Parse first occurencce---");
-    let mut v = match get_real_digit(s, false) {
-        Some(x) => x,
-        None => return c
-    };
-    c.first = v.digit;
-
-    println!("Parse last occurencce---");
-    v = match get_real_digit(s, true) {
-        Some(x) => x,
-        None => return c
-    };
-    c.last = v.digit;
-
-    // print for debugging purposes only
-    println!(">>>> Parsed line: {:?} => {:?} {:?}", s, c.first, c.last);
-
-    // Return the wanted value
-    c
 
 }
 
-fn parse_input_document(s: &str) -> Result<CalibrationDocument, std::io::Error> {
-    let f = match File::open(s) {
-        Ok(k) => k,
-        Err(e) => return Err(e)
-    };
-    let mut c = CalibrationDocument { lines: vec![] };
-    for l in BufReader::new(f).lines()
-        .map(|x| match x {
-            Ok(y) => y,
-            Err(_) => String::from("")
-        }) {
-        /*match parse_input_line(&l) {
-            Some(r) => c.lines.push(r),
-            None => continue
-        };*/
-        c.lines.push(parse_input_line(&l));
-    }
-    Ok(c)
-}
-
+/* *************************************************************************
+                                 MAIN PROGRAM         
+   ************************************************************************* */
 fn main() {
     // Get the input file name
     let input_file = match env::args().nth(1) {
@@ -241,7 +215,7 @@ fn main() {
     };
 
     // Read and parse the input document containing calibration values
-    let input_document: CalibrationDocument = match parse_input_document(&input_file) {
+    let input_document: CalibrationDocument = match CalibrationDocument::new(&input_file, false) {
         Ok(c) => c,
         Err(e) => panic!("ERROR: Failed to read input file {:?}!\n{:?}", input_file, e)
     };
