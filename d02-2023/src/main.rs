ea4e9f81527afc60eb41b1fcde33c153e11bd6a2
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-22 16:16:43 -0500

    Working solution to 2023 Day 02 Part 1

--- d02-2023/src/main.rs
+++ d02-2023/src/main.rs
@@ -2,9 +2,9 @@
 /* *************************************************************************
                            LIBRARIES AND DECLARATIONS           
    ************************************************************************* */
-   use std::env;
-   use std::fs:: File;
-   use std::io::{BufRead, BufReader};
+use std::env;
+use std::fs:: File;
+use std::io::{BufRead, BufReader};
 use std::num::ParseIntError;
 
 
@@ -19,13 +19,32 @@ fn get_game_id(header: &str) -> Result<u16, ParseIntError> {
     number.to_owned().parse::<u16>()
 }
 
+fn parse_set_color(s: &str) -> Option<(String, u32)> {
+    let parts: Vec<&str> = s.trim().split_whitespace().collect();
+    let color = match parts.get(1) {
+        None => return None,
+        Some(c) => {
+            match *c {
+                "red" => "red".to_string(),
+                "green" => "green".to_string(),
+                "blue" => "blue".to_string(),
+                _ => return None
+            }
+        }
+    };
+    let count = match parts.get(0) {
+        Some(c) => c.to_owned().parse::<u32>().unwrap_or(0u32),
+        None => 0u32
+    };
+    return Some((color, count));
+}
 
 /* *************************************************************************
                             STRUCTURE AND METHODS
    ************************************************************************* */
 
 /* -------------------------------------------------------------------------
-   Subset struct and functions
+   Subset (a.k.a. set) struct and functions
    ------------------------------------------------------------------------- */
 struct Subset {
     red: u32,
@@ -33,27 +52,96 @@ struct Subset {
     blue: u32
 }
 
+impl Subset {
+    fn from(b: &str) -> Option<Subset> {
+        let mut z = Subset { red: 0, green: 0, blue: 0 };
+        for c in b.to_lowercase().trim().split(',').into_iter() {
+            match parse_set_color(c) {
+                None => continue,
+                Some((x, y)) => {
+                    match x.as_str() {
+                        "red" => { z.red = y },
+                        "green" => { z.green = y },
+                        "blue" => { z.blue = y },
+                        _ => continue
+                    };
+                }
+            };
+        }
+        Some(z)
+    }
+
+    fn show(&self) {
+        println!("\tred: {:?}, green: {:?}, blue: {:?}", self.red, self.green, self.blue);
+    }
+
+    fn possible(&self, reference: &Subset) -> bool {
+        if self.red > reference.red {
+            false
+        } else if self.green > reference.green {
+            false
+        } else if self.blue > reference.blue {
+            false
+        } else {
+            true
+        }
+    }
+
+}
+
+
 /* -------------------------------------------------------------------------
    Game struct and functions
    ------------------------------------------------------------------------- */
 struct Game {
-    id: u32,
+    id: u16,
     subsets: Vec<Subset>
 }
 
 impl Game {
     fn from(input_line: &str) -> Option<Game> {
+        //Split and lazy check if the game is invalid i.e. more elements after split
         let arr: Vec<&str> = input_line.split(':').collect();
         if arr.len() > 2 {
             return None;
         }
-        let id = match get_game_id(arr[0]) {
+
+        // Get the game number i.e. game id
+        let game_id = match get_game_id(arr[0]) {
             Ok(i) => i,
-            Err(e) => return None
+            Err(_e) => return None
         };
-        let sets: Vec<Subset> = Vec::new();
-        todo!();
+
+        // Get the subset details of the game
+        // Assume a game must have at least one subset
+        let mut sets: Vec<Subset> = Vec::new();
+        for body in arr[1].to_lowercase().split(';') {
+            match Subset::from(body) {
+                Some(b) => sets.push(b),
+                None => continue
+            };
+        }
+   
+        // TODO: Remove this.  Used only for debugging.
+        println!("Game#: {:?}", game_id);
+        for zz in sets.iter() {
+            zz.show();
+        }
+
+        Some(Game { id: game_id, subsets: sets })
     }
+
+    fn possible(&self, r: &Subset) -> (u16, bool) {
+        for s in self.subsets.iter() {
+            if s.possible(r) {
+                continue;
+            } else {
+                return (self.id, false);
+            }
+        } 
+        return (self.id, true);
+    }
+
 }
 
 
@@ -89,5 +177,24 @@ fn main() {
         };
         games.push(g);
     }
+
+    // Test the possible games
+    let max = Subset {
+        red: 12,
+        green: 13,
+        blue: 14
+    };
+    let mut possible_games: Vec<u16> = vec![];
+    let mut sum_possible_games: u32 = 0;
+    for g in games.iter() {
+        if g.possible(&max).1 {
+            possible_games.push(g.id);
+            sum_possible_games += u32::from(g.id);
+        }
+    }
+
+    println!("\nPossible games: {:?}", possible_games);
+    println!("Sum of id's of possible games: {:?}", sum_possible_games);
+
 
 }
