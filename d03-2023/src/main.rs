7459100726113b515f9363f9af24a1e4472cbd83
Erwin Coder 9 <erwincoder9@gmail.com>
2024-09-26 18:11:20 -0500

    Add solution to 2023 day 3 part 2 plus minor tweaks to solution to part 1

--- d03-2023/src/main.rs
+++ d03-2023/src/main.rs
@@ -33,21 +33,23 @@ struct Coordinate {
 /* -------------------------------------------------------------------------
    Symbol struct and functions
    - Symbols can only be a single character
+   - Added outline attribute for part 2, which are coordinates around the symbol
+     Starts as empty Vector.
    ------------------------------------------------------------------------- */
 #[derive(Debug)]
 struct Symbol {
     symbol: char,
-    coordinate: Coordinate,
+    coordinate: Coordinate
 }
 
 impl Symbol {
     // Our init or new method
     fn make_symbol(s: char, c: Coordinate) -> Symbol {
-        Symbol { symbol: s, coordinate: c} 
+        Symbol { symbol: s, coordinate: c }
     }
 
     // Return the possible adjacent coordinates
-    fn adjacent_coordinates(&self, max: Coordinate) -> Vec<Coordinate> {
+    fn adjacent_coordinates(&mut self, max: Coordinate) -> Option<Vec<Coordinate>> {
         // initialize an empty return value
         let mut adjacents: Vec<Coordinate> = vec![];
         // minimum and maximum coordinates adjacent to the symbol
@@ -76,8 +78,9 @@ impl Symbol {
                 }
             }
         }
-        adjacents
+        Some(adjacents)
     }
+
 }
 
 /* -------------------------------------------------------------------------
@@ -96,6 +99,22 @@ impl Number {
         let num: String = d.into_iter().collect();
         Number { number: num.parse::<u32>().unwrap_or(0u32), coordinates: c }
     }
+
+    // check if a vector of coordinates is a coordinate of a number
+    fn has_coordindates(&self, points: &Vec<Coordinate>) -> bool {
+        // TODO: for debugging only remove this
+        //println!("\tNumber: {:?}", self.number);
+        for p in points.iter() {
+            // TODO: for debugging only remove this
+            //println!("\t\t adj point: {:?}", p);
+            for c in self.coordinates.iter() {
+                // TODO: for debugging only remove this
+                //println!("\t\t\t num point: {:?}", c);
+                if c.y == p.y && c.x == p.x { return true };
+            }
+        }
+        false
+    }
 }
 
 /* -------------------------------------------------------------------------
@@ -109,8 +128,8 @@ struct Document {
 
 impl Document {
 
-    // Read a file as our input document and parse each line and each character
-    // in a line.  A document can have symbols and numbers.
+    // Read and parse input file.  A document can have symbols and numbers.
+    // Hence, parse each line and each character in a line.  
     fn make_document(file: File) -> Option<Document> {
         // Initialize our Document object
         let mut document = Document { 
@@ -121,8 +140,7 @@ impl Document {
                 y: 0
             }
         };
-        // Loop through each line and each character in the line to get possible 
-        // symbols and numbers
+        // Loop through each line and character in the line to get possible symbols and numbers
         let mut line_number: u32 = 0; // Our current y value of coordinate
         let mut char_number: u32 = 0; // Our current x value of coordinate
         let mut current_coordinate = Coordinate{ x: char_number, y: line_number };
@@ -145,17 +163,17 @@ impl Document {
                     // if a digit, append the character and coordinates to our buffers.
                     buffer_number_digits.push(character);
                     buffer_number_coordinates.push(current_coordinate);
-                    // if we are the last character in the line,
-                    // we need to flush the buffer before going to the next line
+                    // if we are the last character in the line, flush the buffer before going to the next line.
+                    // using different variable (type usize) instead of char_number (type u32) since we are too
+                    // lazy to deal with converting a type to another and not assuming a pre-determined grid size.
                     if index == line_length {
                         is_flush_buffer = true;
                     }
                 } else if character == '.' {
                     is_flush_buffer = true;
                 } else {
-                    // if a symbol, create a new symbol object and add to the document
-                    // we also set flag to flush buffers as previous character 
-                    // may have been part of a series of digits
+                    // if a symbol, create a new symbol object and add to the document then set flag to flush 
+                    // buffers as previous character may have been part of a series of digits
                     document.symbols.push(
                         Symbol::make_symbol(character, current_coordinate)
                     );
@@ -189,11 +207,15 @@ impl Document {
 
     // The part numbers.  According to the challenge exercise,
     // part numbers are the numbers that are adjacent to a symbol including diagonally.
-    fn part_numbers(&self) -> Vec<u32> {
+    fn part_numbers(&mut self) -> Vec<u32> {
         let mut parts: Vec<u32> = Vec::new();
-        let mut adjacent_points: Vec<Coordinate> = Vec::new();
-        for symbol in self.symbols.iter() {
-            let mut symbol_ajacents = symbol.adjacent_coordinates(self.maxpoint);
+        let mut adjacent_points: Vec<Coordinate> = vec![];
+        for symbol in self.symbols.iter_mut() {
+            symbol.adjacent_coordinates(self.maxpoint);
+            let mut symbol_ajacents = match symbol.adjacent_coordinates(self.maxpoint) {
+                Some(ss) => ss,
+                None => vec![]
+            };
             // TODO: Remvoe this only used for debugging
             //println!("Symbol: {:?} {:?}", symbol.symbol, symbol_ajacents);
             adjacent_points.append(&mut symbol_ajacents);
@@ -214,6 +236,59 @@ impl Document {
             }
         }
         parts
+    }
+
+    // Gears is a collection of Symbols that have a symbol value of '*' and is adjacent to two numbers.
+    fn gear_ratios(&mut self) -> Vec<u32> {
+        let mut ratios: Vec<u32> = vec![];
+        let mut adjacent_numbers: Vec<u32> = vec![];
+        for s in self.symbols.iter_mut() {
+            if s.symbol == '*' {
+                // TODO: for debugging only remove this
+                //println!("Symbol: {:?} at {:?}", s.symbol, s.coordinate);
+                // get the collection of adjacent coordinates of the potential gear
+                let points = match s.adjacent_coordinates(self.maxpoint) {
+                    Some(ss) => ss,
+                    None => vec![]
+                };
+                // TODO: for debugging only remove this
+                //println!("\tAdjacents: {:?}", points);
+                // loop through each number if they're adjacent to the potential gear
+
+                // TODO: make this work
+                /*
+                // for additional efficiency just take only all the numbers above and below the symbol.
+                let line_above = if (s.coordinate.y - 1) == 0 
+                    { s.coordinate.y } else { s.coordinate.y - 1};
+                let line_below= if (s.coordinate.y + 1) > self.maxpoint.y 
+                    { s.coordinate.y } else { s.coordinate.y + 1 };
+                */
+                // assume that a number can only be on one line (its digits does not traverse multiple lines)
+                // TODO: for debugging only remove this
+                //println!("\tLine above: {:?}, below: {:?}", line_above, line_below);
+                for num in self.numbers.iter() {
+                    // TODO: make this work
+                    //if num.coordinates[0].y < line_above || num.coordinates[0].y > line_below { continue };
+                    if num.has_coordindates(&points) { 
+                        adjacent_numbers.push(num.number);
+                        // TODO: for debugging only remove this
+                        //println!("\tNumber: {:?}", num.number);
+                    }
+                }
+                if adjacent_numbers.len() >= 2 { 
+                    let mut product = 1u32;
+                    for nn in adjacent_numbers.iter() {
+                        product *= nn;
+                    }
+                    ratios.push(product);
+                    // TODO: for debugging only remove this
+                    //println!("\tGear parts: {:?} Product {:?} ", adjacent_numbers, product);
+                };
+                adjacent_numbers.clear();
+            }
+        }
+        // placeholder return value
+        ratios
     }
 }
 
@@ -237,13 +312,15 @@ fn main() {
         Err(e) => panic!("ERROR: Cannot open file {:?}!\n{:?}", &input_file, e)
     };
 
-    let document = match Document::make_document(f) {
+    let mut document = match Document::make_document(f) {
         Some(d) => d,
         None => {
             println!("ERROR: Cannot read input file {:?}", &input_file);
             std::process::exit(1);
         }
     };
+
+    // Part 1: Get the sum of the part numbers
     let part_numbers = document.part_numbers();
     // TODO: Remvoe this only used for debugging
     //dbg!(&part_numbers);
@@ -254,6 +331,14 @@ fn main() {
         sum_of_parts += *p;
     }
     println!("Sum of part numbers: {:?}", sum_of_parts);
+
+    // Part 2: Get the sum of the product of the numbers adjacent to gears (symbols = '*')
+    let gear_ratios = document.gear_ratios();
+    // TODO: for debugging only, remove this
+    //println!("Gear ratios: {:?}", gear_ratios);
+    let sum_gear_ratios: u32 = gear_ratios.into_iter().sum();
+    println!("Sum of gear ratios: {:?}", sum_gear_ratios);
+
 
     
 }
